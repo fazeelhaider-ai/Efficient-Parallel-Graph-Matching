36e105e34bd1556cb5a161571f73b8f39bbf8f78
fazeelhaider-ai <56573133+fazeelhaider-ai@users.noreply.github.com>
2021-01-01 15:29:38 -0500

    Update strongestNeighborScan.cu

--- gpu_required/strongestNeighborScan.cu
+++ gpu_required/strongestNeighborScan.cu
@@ -1,9 +1,4 @@
-/*
- **********************************************
- *  CS314 Principles of Programming Languages *
- *  Spring 2020                               *
- **********************************************
- */
+
 #include <stdio.h>
 #include <stdlib.h>
 
